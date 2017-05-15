0e755c14277761c623241b40f8df30f16ed0924a
Bogdan <win2win@protonmail.com>
2017-05-14 23:03:40 -0500

    modify token contract

--- sol/SimpleToken.sol
+++ sol/SimpleToken.sol
@@ -1,6 +1,6 @@
-    pragma solidity ^0.4.6;
-   
-    /// @dev limited token contract
+    pragma solidity ^0.4.1;
+    contract tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData); }
+
     contract MyToken {
         /* Public variables of the token */
         string public standard = 'Token 0.1';
@@ -9,11 +9,10 @@
         uint8 public decimals;
         uint256 public totalSupply;
         address public owner;
-        address public ldAddress;
 
         /* This creates an array with all balances */
         mapping (address => uint256) public balanceOf;
-       
+        mapping (address => mapping (address => uint256)) public allowance;
 
         /* This generates a public event on the blockchain that will notify clients */
         event Transfer(address indexed from, address indexed to, uint256 value);
@@ -23,38 +22,67 @@
             uint256 initialSupply,
             string tokenName,
             uint8 decimalUnits,
-            string tokenSymbol,
-            address liqiuidDemocracyAddress
+            string tokenSymbol
             ) {
             balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens
             totalSupply = initialSupply;                        // Update total supply
             name = tokenName;                                   // Set the name for display purposes
             symbol = tokenSymbol;                               // Set the symbol for display purposes
-            decimals = decimalUnits;   
-            ldAddress = liqiuidDemocracyAddress;
-                                     // Amount of decimals for display purposes
+            decimals = decimalUnits;                            // Amount of decimals for display purposes
           if (!msg.sender.send(msg.value))
           throw;                         // Send back any ether sent accidentally
 
            owner = msg.sender;
         }
-       
-        /// @dev Used to create new tokens. It can be only called by owner or the Liquid Democracy 
-        /// contract
-        /// @param target - address of the member receiving tokens
-        /// @param  mintedAmount - amount of new tokens created. 
-        function mintToken(address target, uint256 mintedAmount)  {
 
-            if (owner != msg.sender ||  ldAddress != msg.sender) throw;
+        /* Send coins */
+        function transfer(address _to, uint256 _value) {
+        // if (balanceOf[msg.sender] < _value) throw;           // Check if the sender has enough
+        // if (balanceOf[_to] + _value < balanceOf[_to]) throw; // Check for overflows
+            balanceOf[msg.sender] -= _value;                     // Subtract from the sender
+            balanceOf[_to] += _value;                            // Add the same to the recipient
+            Transfer(msg.sender, _to, _value);                   // Notify anyone listening that this transfer took place
+        }
+
+        /* Allow another contract to spend some tokens in your behalf */
+        function approveAndCall(address _spender, uint256 _value, bytes _extraData)
+            returns (bool success) {
+            allowance[msg.sender][_spender] = _value;
+            tokenRecipient spender = tokenRecipient(_spender);
+            spender.receiveApproval(msg.sender, _value, this, _extraData);
+            return true;
+        }
+
+        /* A contract attempts to get the coins */
+        function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+            if (balanceOf[_from] < _value) throw;                 // Check if the sender has enough
+            if (balanceOf[_to] + _value < balanceOf[_to]) throw;  // Check for overflows
+        //  if (_value > allowance[_from][msg.sender]) throw;   // Check allowance
+            balanceOf[_from] -= _value;                          // Subtract from the sender
+            balanceOf[_to] += _value;                            // Add the same to the recipient
+            allowance[_from][msg.sender] -= _value;
+            Transfer(_from, _to, _value);
+            return true;
+        }
+
+
+        function mintToken(address target, uint256 mintedAmount)  {
             balanceOf[target] += mintedAmount;
             totalSupply += mintedAmount;
             Transfer(0, target, mintedAmount);
         
         }
 
-         ///@dev destroy the contract when not needed 
+        function clear(address member){
+            //delete this function after before deploying to live environement. 
+            //it is only meant to cealn it when other components are recrated. 
+                totalSupply = 0;
+                balanceOf[member] = 0;
+
+        }
+
          function kill() {
-             if (msg.sender == owner) selfdestruct(owner);
+        if (msg.sender == owner) selfdestruct(owner);
     }
 
         /* This unnamed function is called whenever someone tries to send ether to it */
